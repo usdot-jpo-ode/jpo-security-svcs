5758f73c254cc8c1667a84d8d6876d2ca3cfc435
Matthew Schwartz <schwartz_matthew2@bah.com>
2018-03-30 10:50:58 -0400

    Added configuration properties and added hsm/external service toggle

--- src/main/java/controllers/SignatureController.java
+++ src/main/java/controllers/SignatureController.java
@@ -2,6 +2,9 @@ package controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,16 +19,23 @@ import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@Configuration
+@PropertySource("classpath:application.properties")
 @RestController
 public class SignatureController {
 
+   @Value("${destIp}")
+   private static String destIp;
+   @Value("${destPort}")
+   private static String destPort;
+   @Value("${mockResponse}")
+   private static boolean mockResponse;
+   @Value("${useHsm}")
+   private static boolean useHsm;
+
    public static class Message {
       @JsonProperty("message")
-      public String message;
-      @JsonProperty("ip")
-      public String ip;
-      @JsonProperty("port")
-      public String port;
+      public String msg;
    }
 
    private static final Logger logger = LoggerFactory.getLogger(SignatureController.class);
@@ -34,14 +44,26 @@ public class SignatureController {
    @ResponseBody
    public ResponseEntity<String> sign(@RequestBody Message message) {
 
-      logger.info("Received message: {}", message.message);
-      logger.info("Destination ip:port  {}:{}", message.ip, message.port);
+      logger.info("Received message: {}", message.msg);
 
-      ResponseEntity<String> result = forwardMessageToGreenHills(message);
+      ResponseEntity<String> response = null;
 
-      logger.info("Received response: {}", result);
+      if (mockResponse) {
+         logger.info("Returning mock response");
+         response = ResponseEntity.status(HttpStatus.OK).body(jsonPair("result", message.msg + "_signature"));
+      } else if (useHsm) {
+         logger.info("Signing using HSM");
+         response = signWithHsm(message);
+      } else {
+         logger.error("Sending signature request to external service");
+         ResponseEntity<String> result = forwardMessageToExternalService(message);
 
-      return ResponseEntity.status(HttpStatus.OK).body(jsonPair("result", result.getBody()));
+         logger.info("Received response: {}", result);
+
+         response = ResponseEntity.status(HttpStatus.OK).body(jsonPair("result", result.getBody()));
+      }
+
+      return response;
 
    }
 
@@ -49,25 +71,30 @@ public class SignatureController {
       return "{\"" + key + "\":\"" + value + "\"}";
    }
 
-   private ResponseEntity<String> forwardMessageToGreenHills(Message message) {
+   private ResponseEntity<String> forwardMessageToExternalService(Message message) {
+
       HttpHeaders headers = new HttpHeaders();
       headers.setContentType(MediaType.APPLICATION_JSON);
 
-      HttpEntity<String> entity = new HttpEntity<String>(jsonPair("message", message.message), headers);
+      HttpEntity<String> entity = new HttpEntity<>(jsonPair("message", message.msg), headers);
 
       RestTemplate template = new RestTemplate();
 
       logger.info("Rest request: {}", entity);
 
-      String uri = "http://" + message.ip + ":" + message.port + "/tmc/signtim";
+      String uri = "http://" + destIp + ":" + destPort + "/tmc/signtim";
+      logger.info("Destination ip:port  {}:{}", destIp, destPort);
       logger.info("URI: {}", uri);
 
-      ResponseEntity<String> respEntity = template.postForEntity(uri, jsonPair("message", message.message),
-            String.class);
+      ResponseEntity<String> respEntity = template.postForEntity(uri, jsonPair("message", message.msg), String.class);
 
       logger.info("Rest response: {}", respEntity);
 
       return respEntity;
+   }
+
+   private ResponseEntity<String> signWithHsm(Message message) {
+      return ResponseEntity.status(HttpStatus.OK).body(jsonPair("result", message + "_signature"));
    }
 
 }
