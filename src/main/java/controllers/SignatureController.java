d6948588f30ee7b0b6ada370379fc6115dbbf945
572682 <musavi_hamid@bah.com>
2018-04-10 14:05:36 -0400

    ODE-738 changed properties to specify the URI and Path instead of IP/Port/Path. This will allow the user to use whatever protocol, http or https.

--- src/main/java/controllers/SignatureController.java
+++ src/main/java/controllers/SignatureController.java
@@ -31,18 +31,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @RestController
 public class SignatureController {
    
-   private static final String MOCK_MESSAGE = "03810040038081a3d34d45e80ef4db807dd35102f42db7e81d34d34d34d34d34d05efbe43f41d37d35100ef7138e760f5e77f7bd7a0bdf44f43e79e75d34d34dc5d00e7d074f34d35d37d3b17c000f7defd1780f7041dc0d00f76eba0b4d34d34d3ce781370760b8ef6f75176d44f39104134e7bf7cd34dbce36d02e45dbad7bd79e3617c14407c13cd7b0bcdc30ba17c044e35d84dfc0b9176d03ef50b8db5f34d34db4d770c30fbeba0b60018300019924e7b3b2720001992842024272810101000301801631afb5fc255d0f508208f49317071422d1925e6f5b00031acb5dbc8400a983010180034801010001838182792f4e20404c92bf0707999b338ef65e6d6f110bfbf1b67a360ed8a8e412bfa88083a83da9c99739b68f2eff338bbb4b9af2982fe50d843f0f896b9cf291e5d39d1417be0d856eaaea639de2f6ff2d42928e0e2374cbe1ac5dc0d065b0a36ecdfac6";
+//   private static final String MOCK_MESSAGE = "03810040038081a3d34d45e80ef4db807dd35102f42db7e81d34d34d34d34d34d05efbe43f41d37d35100ef7138e760f5e77f7bd7a0bdf44f43e79e75d34d34dc5d00e7d074f34d35d37d3b17c000f7defd1780f7041dc0d00f76eba0b4d34d34d3ce781370760b8ef6f75176d44f39104134e7bf7cd34dbce36d02e45dbad7bd79e3617c14407c13cd7b0bcdc30ba17c044e35d84dfc0b9176d03ef50b8db5f34d34db4d770c30fbeba0b60018300019924e7b3b2720001992842024272810101000301801631afb5fc255d0f508208f49317071422d1925e6f5b00031acb5dbc8400a983010180034801010001838182792f4e20404c92bf0707999b338ef65e6d6f110bfbf1b67a360ed8a8e412bfa88083a83da9c99739b68f2eff338bbb4b9af2982fe50d843f0f896b9cf291e5d39d1417be0d856eaaea639de2f6ff2d42928e0e2374cbe1ac5dc0d065b0a36ecdfac6";
 
-   @Value("${sec.destIp}")
-   public String destIp;
-   @Value("${sec.destPort}")
-   public int destPort;
-   @Value("${sec.mockResponse}")
-   public boolean mockResponse;
+   @Value("${sec.cryptoServiceBaseUri}")
+   public String cryptoServiceBaseUri;
+   @Value("${sec.cryptoServiceEndpointSignPath}")
+   private String cryptoServiceEndpointSignPath;
+//   @Value("${sec.mockResponse}")
+//   public boolean mockResponse;
    @Value("${sec.useHsm}")
    public boolean useHsm;
-   @Value("${sec.signPath}")
-   private String signPath;
 
    public static class Message {
       @JsonProperty("message")
@@ -59,13 +57,14 @@ public class SignatureController {
 
       ResponseEntity<Map<String,String>> response = null;
       
-      logger.info("mockResponse == {}", mockResponse);
-
-      if (mockResponse) {
-         logger.info("Returning mock response");
-         response = ResponseEntity.status(HttpStatus.OK).body(
-            Collections.singletonMap("result", MOCK_MESSAGE));
-      } else if (useHsm) {
+//      logger.info("mockResponse == {}", mockResponse);
+//
+//      if (mockResponse) {
+//         logger.info("Returning mock response");
+//         response = ResponseEntity.status(HttpStatus.OK).body(
+//            Collections.singletonMap("result", MOCK_MESSAGE));
+//      } else if (useHsm) {
+      if (useHsm) {
          logger.info("Signing using HSM");
          response = signWithHsm(message);
       } else {
@@ -94,7 +93,17 @@ public class SignatureController {
 
       logger.info("Received request: {}", entity);
 
-      URI uri = new URI("http", null, destIp, destPort, signPath, null, null);
+      //Remove all slashes from the end of the URI, if any
+      while (cryptoServiceBaseUri != null && cryptoServiceBaseUri.endsWith("/")) {
+         cryptoServiceBaseUri = cryptoServiceBaseUri.substring(0, cryptoServiceBaseUri.lastIndexOf('/'));
+      }
+
+      //Remove all slashes from the beginning of the path string, if any
+      while (cryptoServiceEndpointSignPath != null && cryptoServiceEndpointSignPath.startsWith("/")) {
+         cryptoServiceEndpointSignPath = cryptoServiceEndpointSignPath.substring(1);
+      }
+
+      URI uri = new URI(cryptoServiceBaseUri + "/" + cryptoServiceEndpointSignPath);
       
       logger.info("Sending request to: {}", uri);
 
