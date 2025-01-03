3ca8b2da0a886f5a53ae98290c382878624c238b
Matt Cook <matt.cook@trihydro.com>
2025-01-03 09:45:26 -0700

    feat: add mock signing service project skeleton
    Initial setup for a new Spring Boot mock signing service project. Includes Maven wrapper, project dependencies, configuration files, basic application entry point, and a test class.

---
+++ ext-signature-svc-mock/src/main/java/us/dot/its/jpo/sec/mock/signer/MockSigningServiceApplication.java
@@ -0,0 +1,13 @@
+package us.dot.its.jpo.sec.mock.signer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockSigningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockSigningServiceApplication.class, args);
+	}
+
+}
---
+++ ext-signature-svc-mock/src/test/java/us/dot/its/jpo/sec/mock/signer/MockSigningServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package us.dot.its.jpo.sec.mock.signer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockSigningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// TODO
+	}
+
+}
