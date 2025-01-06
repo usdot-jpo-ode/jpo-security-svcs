31f1cbc8d905b4e6754e6d62d665e065530bec2c
Matt Cook <matt.cook@trihydro.com>
2025-01-06 15:47:34 -0700

    test: removed incorrect RunWith annotation

--- security-svc/src/test/java/us/dot/its/jpo/sec/controllers/SignatureExceptionHandlerTest.java
+++ security-svc/src/test/java/us/dot/its/jpo/sec/controllers/SignatureExceptionHandlerTest.java
@@ -1,14 +1,11 @@
 package us.dot.its.jpo.sec.controllers;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 class SignatureExceptionHandlerTest {
 
     @Test
